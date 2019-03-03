66b241828a5232ee56ca304e0cc9473f2c4c2914
Thomas Groch <thomas.groch@gmail.com>
2019-03-02 23:04:38 -0300

    better ping, script fixed

--- scripts/check-if-i-am-reachable.sh
+++ scripts/check-if-i-am-reachable.sh
@@ -4,4 +4,4 @@
 # - requires: curl
 # - output: OK if IP is up, KO if not.
 IP=`curl -s 'https://api.ipify.org'` && \
-  ( curl -s "http://www.isup.me/$IP" | grep -cq "It's just you" && echo 'OK' ) || echo 'KO'
+  ( curl -s "https://downforeveryoneorjustme.com/$IP" | grep -cq "It's just you" && echo 'yes' ) || echo 'no'
