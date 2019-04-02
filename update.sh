1efc71a4a9aadc0b5c8cb175b336dc1f1c4d65cc
Thomas Groch <thomas.groch@gmail.com>
2019-04-02 18:24:44 -0300

    now updating casks too

--- update.sh
+++ update.sh
@@ -19,7 +19,6 @@ trap finish EXIT
 # Use python3 by default
 rm -f /usr/local/bin/python 2>&1 >/dev/null; ln -s $(which python3) /usr/local/bin/python
 
-
 # stdout to null, stderr to stdout
 pip3 install --upgrade setuptools 2>&1 >/dev/null;	code=$?; [[ code -ne 0 ]] && echo "pip setuptools update; exit code was ${code}"
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U 2>&1 >/dev/null;	code=$?; [[ code -ne 0 ]] && echo "pip3 update; exit code was ${code}"
@@ -29,7 +28,18 @@ brew upgrade 2>&1 >/dev/null;			code=$?; [[ code -ne 0 ]] && echo "brew upgrade;
 brew cask upgrade 2>&1 >/dev/null;		code=$?; [[ code -ne 0 ]] && echo "cask upgrade; exit code was ${code}"
 mas upgrade 2>&1 >/dev/null;			code=$?; [[ code -ne 0 ]] && echo "mas upgrade; exit code was ${code}"
 tldr --update 2>&1 >/dev/null;			code=$?; [[ code -ne 0 ]] && echo "tldr update; exit code was ${code}"
+
+casks_outdated=`brew cask outdated --greedy --verbose | grep -v '(latest)' | cut -d' ' -f1`
+delete=(webstorm phpstorm)
+for del in ${delete[@]}
+do
+   casks_outdated=("${casks_outdated[@]/$del}")
+done
+brew cask upgrade `echo $casks_outdated` 2>&1 >/dev/null;		code=$?; [[ code -ne 0 ]] && echo "custom cask upgrade; exit code was ${code}"
+
+# cleans up cached downloads (with --outdated, only cleans old downloads)
 brew cleanup 2>&1 >/dev/null;			code=$?; [[ code -ne 0 ]] && echo "brew cleanup; exit code was ${code}"
+# checks for configuration issues
 brew doctor 2>&1 >/dev/null;			code=$?; [[ code -ne 0 ]] && echo "brew doctor; exit code was ${code}"
 
 # Local Notification
@@ -40,11 +50,11 @@ TEXT=$(printf "\
 ==============\n \
 HOSTNAME: "`hostname`"\n\
 WAN IP: "`dig +short myip.opendns.com @resolver1.opendns.com`"\n\
-\n\
+\n\n\
 Update History Log:\n\
 `cat ~/logs/"$(ls -rt ~/logs/ | tail -n1)"`\
 ")
-npx --quiet telemify "${TEXT}"
+# npx --quiet telemify "${TEXT}"
 
 # Altenative way to send
 # JSON_CONFIG="$(cat ~/.config/telemify/config.json)"
