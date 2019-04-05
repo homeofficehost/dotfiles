f2a26c1b9a243afc4ab51ae37bd40c525767a83a
Thomas Groch <thomas.groch@gmail.com>
2019-04-04 23:12:00 -0300

    tested

--- scripts/create-udf.sh
+++ scripts/create-udf.sh
@@ -3,9 +3,13 @@
 volumeName=${1:-'Untitled'}
 identifier=${2:-'disk3'}
 
+# TODO: Detect identifier by name
+diskutil list
+
 echo "Creating UDF partition: /Volumes/$volumeName Identifier: $identifier ";
 read -n 1 -s -r -p "Press any key to continue"
 format-udf $identifier "${volumeName}"
+# TODO: Auto unmount
 
 #####################
 # Reference:
