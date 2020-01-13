2ef5fd86465e54a549effc3941b4a77320360e32
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-01-13 13:35:13 -0300

    fix

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # This is the main install script, it runs the system specific installer script
 
-bash ./lib_sh/echos.sh
-bash ./lib_sh/requirers.sh
+source ./lib_sh/echos.sh
+source ./lib_sh/requirers.sh
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  bash ./install-darwin.sh
+  . ./install-darwin.sh
 elif [[ "$(uname -s)" == "Linux" ]]; then
-  bash ./install-linux.sh
+  . ./install-linux.sh
 fi
