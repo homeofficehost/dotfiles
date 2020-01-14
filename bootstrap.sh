343219c30c07ddd71eb683bda2e8871eae313d45
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-01-14 17:29:08 -0300

    bootstrap script updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,32 +3,37 @@ BARE_DOTFILES=$HOME/.bare-dotfiles
 BARE_DOTFILES_BACKUP=$HOME/.bare-dotfiles-bkp
 BARE_DOTFILES_TMP=$HOME/.bare-dotfiles-tmp
 
-
 [[ ! -e .gitmodules ]] && git clone --branch bare --separate-git-dir=$BARE_DOTFILES https://github.com/thomasgroch/dotfiles.git $BARE_DOTFILES_TMP && \
 	cp $BARE_DOTFILES_TMP/.gitmodules $HOME
 rm -rf $BARE_DOTFILES_TMP
 
 [[ ! -e $BARE_DOTFILES ]] && git clone --branch bare --bare https://github.com/thomasgroch/dotfiles.git $BARE_DOTFILES
-# alias dotfiles="git --git-dir=$BARE_DOTFILES --work-tree=$HOME"
 function dotfiles {
    /usr/bin/git --git-dir=$BARE_DOTFILES --work-tree=$HOME $@
 }
-
-mkdir -p $BARE_DOTFILES_BACKUP
+# alias dotfiles="git --git-dir=$BARE_DOTFILES --work-tree=$HOME"
 
 # dotfiles remote add origin https://github.com/thomasgroch/dotfiles.git
 # dotfiles checkout --track bare/bare
 # dotfiles checkout bare
 # dotfiles branch --set-upstream-to=origin/bare bare
 
+# Reset current HEAD to the specified state
+dotfiles reset --hard
+
+# Remove untracked files from the working tree
+# dotfiles clean -df # Caution
+
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dot files.";
+    mkdir -p $BARE_DOTFILES_BACKUP
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BARE_DOTFILES_BACKUP/{}
 fi;
 dotfiles checkout
+
 
 dotfiles config --local status.showUntrackedFiles no
 [[ ! -h .git ]] && ln -s $BARE_DOTFILES .git
