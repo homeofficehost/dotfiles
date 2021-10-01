8c3d2eefa3a0987d4ea85bdeec77d2ca9ca72bcd
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-10-01 14:10:40 -0300

    fix

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,5 +1,9 @@
-if [[ -z $(which ansible-pull) ]]; then
-	sudo pacman -S --noconfirm ansible
+if [[ -z $(which ansible-pull) ]]; then # if are not installed
+	if [[ -n $(which pacman) ]]; then # if are installed
+		sudo pacman -S --noconfirm ansible
+	elif [[ -n $(which apt) ]]; then
+		sudo apt install -y ansible
+	fi
 fi
 if [[ -n $(which ansible-pull) ]]; then
 	sudo ansible-pull --url https://github.com/thomasgroch/dotfiles --limit $(cat /etc/hostname).local
