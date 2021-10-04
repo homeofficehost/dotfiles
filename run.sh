d579f33954fc6c77bbb94811f468caf9adfc2a62
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-10-04 00:06:32 -0300

    vault with gopass client added

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 #     localhost' > inventory.yml
 # ansible-playbook local.yml --inventory inventory.yml --ask-become-pass
 
-ansible-playbook local.yml --ask-become-pass
+ansible-playbook local.yml --vault-id ansible/workstation/tg@gopass-client.py
