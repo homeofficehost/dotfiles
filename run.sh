6ed72fd8b6f26cc5af430d7d0f0979654fa4c415
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-10-15 00:20:57 -0300

    removing zombie code

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-# grep -s -q "localhost" inventory.yml || \
-# echo '
-# all:
-#   hosts:
-#     localhost' > inventory.yml
-# ansible-playbook local.yml --inventory inventory.yml --ask-become-pass
 
 ansible-playbook -c local local.yml --vault-id ansible/workstation/tg@gopass-client.py --limit $(cat /etc/hostname).local
