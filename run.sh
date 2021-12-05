1cd48801f6c64682884a37ed709e31de31b18b36
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-05 19:49:51 -0300

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -c local local.yml --vault-id ansible/workstation/tg@gopass-client.py --limit $(cat /etc/hostname).local
+sudo ansible-playbook -c local local.yml --vault-id ansible/workstation/tg@gopass-client.py --limit $(cat /etc/hostname).local
