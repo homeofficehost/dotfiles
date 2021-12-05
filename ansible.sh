9eee64fa09cde9e942fbf837c7ced4814b3506ab
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-05 19:38:19 -0300

    update

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-pull --url https://github.com/thomasgroch/dotfiles --vault-id ansible/workstation/tg@gopass-client.py  --accept-host-key --limit $(cat /etc/hostname).local --ask-vault-pass
+sudo ansible-pull --url https://github.com/thomasgroch/dotfiles --vault-id ansible/workstation/tg@gopass-client.py  --accept-host-key --limit $(cat /etc/hostname).local --ask-vault-pass
