0f5a3a7bed1b15085a142f3fc49018c52841ae5b
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-05 19:22:49 -0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,52 +1,23 @@
-# Edit this line to set your own remote
-THIS_REPO=https://github.com/thomasgroch/dotfiles.git
+#!/bin/sh
 
-# where $BARE_DOTFILES directory is a git bare repository.
-BARE_DOTFILES=$HOME/.bare-dotfiles
-BARE_DOTFILES_BACKUP=$HOME/.bare-dotfiles-bkp
-BARE_DOTFILES_TMP=$HOME/.bare-dotfiles-tmp
+echo "To import my GnuPG and OpenSSH keys run:"
+echo ". /run/media/tg/safe/run.sh"
+read -n 1 -s -r -p "Press any key to continue"
 
-[[ ! -e .gitmodules ]] && git clone --depth 1 --branch master --separate-git-dir=$BARE_DOTFILES $THIS_REPO $BARE_DOTFILES_TMP && \
-	cp $BARE_DOTFILES_TMP/.gitmodules $HOME
-rm -rf $BARE_DOTFILES_TMP
-
-[[ ! -e $BARE_DOTFILES ]] && git clone --jobs 10 --branch master --bare $THIS_REPO $BARE_DOTFILES
-function dotfiles {
-   /usr/bin/git --git-dir=$BARE_DOTFILES --work-tree=$HOME $@
-}
-
-# dotfiles remote add origin $THIS_REPO
-# dotfiles checkout --track master/bare
-# dotfiles checkout master
-# dotfiles branch --set-upstream-to=origin/master master
-
-# Reset current HEAD to the specified state
-dotfiles reset --hard
-
-# Init submodules
-dotfiles submodule update --init --recursive
-
-# Remove untracked files from the working tree
-# dotfiles clean -df # Caution
-
-dotfiles checkout
-if [ $? = 0 ]; then
-	echo "Checked out dotfiles.";
-else
-	echo "Backing up pre-existing dot files.";
-	mkdir -p $BARE_DOTFILES_BACKUP
-	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BARE_DOTFILES_BACKUP/{}
-fi;
-dotfiles checkout
-
-
-dotfiles config --local status.showUntrackedFiles no
-[[ ! -h .git ]] && ln -s $BARE_DOTFILES .git
-
-source ~/.profile
-
-bash ./install.sh
-
-# password-store-installer
-# rsync --recursive --verbose --exclude '.git' dotfiles-tmp/ $HOME/
-# rm --recursive dotfiles-tmp
+if [[ ! -e ~/.password-store ]]; then
+	git clone https://gitlab.com/thomas.groch/password-store.git ~/.password-store
+fi
+if [[ -z $(which pass) ]]; then # if are not installed
+	if [[ -n $(which pacman) ]]; then # if are installed
+		sudo pacman -S --noconfirm pass
+	elif [[ -n $(which apt) ]]; then
+		sudo apt install -y pass
+	fi
+fi
+if [[ -z $(which ansible-pull) ]]; then # if are not installed
+	if [[ -n $(which pacman) ]]; then # if are installed
+		sudo pacman -S --noconfirm ansible
+	elif [[ -n $(which apt) ]]; then
+		sudo apt install -y ansible
+	fi
+fi
