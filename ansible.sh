cdbdef0e11a026f22a942f1f87e30a59366d3548
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-08 19:18:27 -0300

    update

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+pass show "ansible/workstation/${USER}"
 ansible-pull --url https://github.com/thomasgroch/dotfiles --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
