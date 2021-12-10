70b310b80e4de82475248d511969a1bd042f3ff5
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-10 19:38:01 -0300

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo ansible-playbook -c local local.yml --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local --ask-become-pass
+ansible-playbook -c local local.yml --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
