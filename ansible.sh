f20a222e3a1ad086f7d78c71a28280ecccc4dc92
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-12-18 22:12:14 -0300

    update dotfiles submodules bootstrap

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pass show "ansible/workstation/${USER}"
-ansible-pull --url https://github.com/thomasgroch/dotfiles --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local --track-subs
+ansible-pull --url https://github.com/thomasgroch/dotfiles --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
