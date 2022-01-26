d2832963f9aa321715c1fe09c81d13353e9b98b7
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-01-26 17:44:20 -0300

    update

--- gopass-client.py
+++ gopass-client.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 #
 # Assuming you have a working gopass installation. I personally use multiple mounts and I store all the passwords under a
 # single directory.
@@ -50,8 +49,6 @@
 #
 # If someone knows how to make config manager work like in the example client in the ansible directory let me know and I will
 # happily make the adjustments.
-
-# -*- coding: utf-8 -*-
 
 import sys
 import argparse
