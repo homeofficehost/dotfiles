fa4a674ade20d514c299762c75f2a4c7e0248211
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-06-29 19:26:53 -0300

    test

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-pass show "ansible/workstation/${USER}"
-ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
+if [[ $USER == *"workstation"* ]]; then
+	pass show "ansible/workstation/${USER}"
+	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
+else
+	ansible-pull --url https://github.com/homeofficehost/dotfiles
+fi
+
