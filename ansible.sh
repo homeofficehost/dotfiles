6816c4831a02f2b51c899da1a7f3f72c9556fc99
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-06-29 19:46:05 -0300

    up

--- ansible.sh
+++ ansible.sh
@@ -4,6 +4,6 @@ if [[ $USER == *"workstation"* ]]; then
 	pass show "ansible/workstation/${USER}"
 	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
 else
-	ansible-pull --url https://github.com/homeofficehost/dotfiles --limit runner
+	ansible-pull --url https://github.com/homeofficehost/dotfiles --limit tgworkstation.local
 fi
 
