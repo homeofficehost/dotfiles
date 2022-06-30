907a341058638fd185c4e7dcbd08fbe2fab69e19
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-06-30 17:34:01 -0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@
 LOCAL_USER=$2
 # Restore pass
 PASSWORD_STORE_REPO=$1
+echo -n "${GPG_SIGNING_KEY}" | base64 --decode | gpg --import
+
 if [[ ! -e ~/.password-store ]]; then
 	git clone "$PASSWORD_STORE_REPO" ~/.password-store
 fi
