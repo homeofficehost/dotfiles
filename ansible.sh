fd5f8d8afe0965e0dbb35d90c349f1e528d1c3f9
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-07-03 13:34:07 -0300

    update

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ $USER == *"workstation"* ]]; then
+if [[ "$(cat /etc/hostname)" == *"workstation"* ]]; then
 	pass show "ansible/workstation/${USER}"
 	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
 else
