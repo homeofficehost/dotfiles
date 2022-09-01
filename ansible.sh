288455dcebf89e48badfe44b262b441120ab05a3
Thomas Letsch Groch <thomas.groch@gmail.com>
2022-09-01 09:42:10 -0300

    limit removed

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@
 # Run playbook
 if [[ "$(cat /etc/hostname)" == *"workstation"* ]]; then
 	pass show "ansible/workstation/${USER}"
-	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
+	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py"
 else
 	pass show "ansible/workstation/runner"
 	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "runner@gopass-client.py" --limit runner
