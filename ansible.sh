c55b0f0ffb79219cac04f851f82247046eefd6f0
Thomas Letsch Groch <thomasgroch@users.noreply.github.com>
2022-09-27 14:57:00 -0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@
 # Run playbook
 if [[ "$(cat /etc/hostname)" == *"workstation"* ]]; then
 	pass show "ansible/workstation/${USER}"
-	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "ansible/workstation/${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
+	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "${USER}@gopass-client.py" --limit $(cat /etc/hostname).local
 else
 	pass show "ansible/workstation/runner"
 	ansible-pull --url https://github.com/homeofficehost/dotfiles --vault-id "runner@gopass-client.py" --limit runner
